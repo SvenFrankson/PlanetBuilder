9cc65a5cbbc1bfa913d50d83a0f62355971e62e4
SvenFrankson <sven.frankson@gmail.com>
2017-05-17 20:51:05 +0200

    Use probability curves to determine Block data.

---
+++ Assets/PlanetBuilder/Scripts/CubeMapGenerator/BlockCurves.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCurves : MonoBehaviour {
+
+	public AnimationCurve SandProba;
+	public AnimationCurve SnowProba;
+	public AnimationCurve RockProba;
+	public AnimationCurve DirtProba;
+}
